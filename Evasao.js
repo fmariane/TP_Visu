a781d2f313122300a8101b5c87bc0ecb759317f1
Carolina Coimbra <Carolina Coimbra>
2016-05-29 21:05:08 -0300

    Tabelas csv e xlsx

--- Evasao.js
+++ Evasao.js
@@ -1,156 +1,126 @@
-///////////////////////////////////
-////Pré processamento de dados////
-///////////////////////////////////
+var margem = {top: 50, right: 0, bottom: 100, left: 240};
+var largura = 1000 - margem.left - margem.right; 
+var altura = 480 - margem.top - margem.bottom;
+var grid = Math.floor(largura/24),
+var larguraLegenda = grid*2,
+var itensLegenda = 9,
+var cores = ["#E20000", "#FF6666", "#FF9999","#FFCBCB","#FFFFFF","#1D91C0","#225EA8","#253494","#081D58"], // alternatively colorbrewer.YlGnBu[9]
+var nomeCursos = ["Ensino Fundamental 2º Ano", "Ensino Fundamental 3º Ano", "Ensino Fundamental 4º Ano", "Ensino Fundamental 5º Ano", "Ensino Fundamental 6º Ano", "Ensino Fundamental 7º Ano", "Ensino Fundamental 8º Ano", "Ensino Fundamental 9º Ano", "Ensino Médio - 1ª Série", "Ensino Médio - 2ª Série", "Ensino Médio - 3ª Série"];
+var dados = ["evasaoNorte.csv", "evasaoNordeste.csv", "evasaoCentroOeste.csv", "evasaoSudeste.csv", "evasaoSul.csv"];
+  
 
-d3.csv("data/evasao.xlsx", function(error, data) {
-  console.log(data);
+//////////////////////////////////////
+//// PRÉ PROCESSAMENTO DOS DADOS ////
+////////////////////////////////////
+d3.csv("data/evasaoNorte.csv", function(error, data){
+  
+  if (error) 
+    throw error;
 
-  data.forEach(function(d)) {
-    //Conversão de tipo +
-    d.evasao = +d.evasao;
-    console.log(d);
-  });
-
-  var maximo = d3.max(data, function(d) { return d.evasao; });
-  var minimo = d3.min(data, function(d) { return d.evasao; });
-  var dominio = d3.extent(data, function(d) { return d.evasao; });
-  console.log(maximo);
-  console.log(minimo);
-  console.log(dominio);
-
-  //Ordenar em ordem crescente, a-b e em ordem decrescente, b-a
-  data.sort(function(a, b) { return a.evasao - b.evasao;  });
-  console.log(data);
-
-  //O container é o id da div no HTML onde a visualização ficará
-  CriaHeatMap(data, "container")
-
+  data.forEach(function(d){
+    d.ano = +d.ano; // Converte coluna para número
+    d.nomeCurso = +d.nomeCurso; // Converte coluna para número
+    d.evasao = +d.evasao; // Converte coluna para número    
+  }); 
+  
+  Main(data);
+  
 });
 
-//////////////////////////////////
-////Criação das visualizações////
-//////////////////////////////////
 
-function CriaHeatMap(dados, container){
+////////////////////////////////////
+//// CRIAÇÃO DAS VISUALIZAÇÕES ////
+//////////////////////////////////
+function CriaHeatMap(dados, container) {
+  
   var svg = d3.select(container).append("svg")
-    .attr ("width", 640)
-    .attr ("height", 480)
-    .style("background-color", "#383838");
-
-  //Criação dos retângulos
-  svg.selectAll("rect")
-    .data(dados)
+    .attr("width", largura + margem.left + margem.right)
+    .attr("height", altura + margem.top + margem.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margem.left + "," + margem.top  + ")");
+    
+  var anoLabels = svg.selectAll(".anoLabel")
+    .data(anos)
     .enter()
-    .append("rect")
-    .attr("width", 30)
-    //Para não começar no zero zero (esquerda em cima)
-    .attr("height", function(d) { return 480 - d.matriculas; })
-    .attr("x", function(d, i) { return i*30; })
-    .attr("y", function(d) { return d.matriculas; })
-    .style ("fill", "#00DEF2");
+    .append(".anoLabel");
+
+  var nomeCursoLabels = svg.select(".nomeCursoLabel")
+    .data(nomeCursos)
+    .enter()
+    .append(".nomeCursoLabel");
+
+  var colorScale = d3.scale.quantile()
+    .domain([0, itensLegenda-1, d3.max(dados, function(d){  return d.evasao;})])
+    .range(cores);
+
+  var cards = svg.selectAll(".nomeCurso")
+    .data(data, function(d) {return d.ano+':'+d.nomeCurso;});
 
 }
 
 
+////////////////////////////////////
+//// DESENHO DAS VISUALIZAÇÕES ////
 //////////////////////////////////
-////Desenho das visualizações////
-//////////////////////////////////
-
-/*
-
-(function(){
-  //UI configuration
-  var itemSize = 18,
-    cellSize = itemSize-1,
-    width = 800,
-    height = 800,
-    margin = {top:20,right:20,bottom:20,left:25};
-
-  //formats
-  var hourFormat = d3.time.format('%H'),
-    dayFormat = d3.time.format('%j'),
-    timeFormat = d3.time.format('%Y-%m-%dT%X'),
-    monthDayFormat = d3.time.format('%m.%d');
-
-  //data vars for rendering
-  var dateExtent = null,
-    data = null,
-    dayOffset = 0,
-    colorCalibration = ['#f6faaa','#FEE08B','#FDAE61','#F46D43','#D53E4F','#9E0142'],
-    dailyValueExtent = {};
-
-  //axises and scales
-  var axisWidth = 0 ,
-    axisHeight = itemSize*24,
-    xAxisScale = d3.time.scale(),
-    xAxis = d3.svg.axis()
-      .orient('top')
-      .ticks(d3.time.days,3)
-      .tickFormat(monthDayFormat),
-    yAxisScale = d3.scale.linear()
-      .range([0,axisHeight])
-      .domain([0,24]),
-    yAxis = d3.svg.axis()
-      .orient('left')
-      .ticks(5)
-      .tickFormat(d3.format('02d'))
-      .scale(yAxisScale);
-
-  initCalibration();
-
-  var svg = d3.select('[role="heatmap"]');
-  var heatmap = svg
-    .attr('width',width)
-    .attr('height',height)
-  .append('g')
-    .attr('width',width-margin.left-margin.right)
-    .attr('height',height-margin.top-margin.bottom)
-    .attr('transform','translate('+margin.left+','+margin.top+')');
-  var rect = null;
-
-
-  function initCalibration(){
-    d3.select('[role="calibration"] [role="example"]').select('svg')
-      .selectAll('rect').data(colorCalibration).enter()
-    .append('rect')
-      .attr('width',cellSize)
-      .attr('height',cellSize)
-      .attr('x',function(d,i){
-        return i*itemSize;
-      })
-      .attr('fill',function(d){
-        return d;
-      });
-
-    //bind click event
-    d3.selectAll('[role="calibration"] [name="displayType"]').on('click',function(){
-      renderColor();
-    });
-  }
-
-  function renderColor(){
-    var renderByCount = document.getElementsByName('displayType')[0].checked;
-
-    rect
-      .filter(function(d){
-        return (d.value['PM2.5']>=0);
-      })
-      .transition()
-      .delay(function(d){      
-        return (dayFormat(d.date)-dayOffset)*15;
-      })
-      .duration(500)
-      .attrTween('fill',function(d,i,a){
-        //choose color dynamicly      
-        var colorIndex = d3.scale.quantize()
-          .range([0,1,2,3,4,5])
-          .domain((renderByCount?[0,500]:dailyValueExtent[d.day]));
-
-        return d3.interpolate(a,colorCalibration[colorIndex(d.value['PM2.5'])]);
-      });
-  }
+function DesenhaHeatMap(container) {  
+  var dados = d3.select(container)
+    .selectAll("rect")
+    .data();
+    
+  var svg = d3.select(container)
+    .select("svg g");
+    
+  var valorMaximo = d3.max(dados, function(d){ return d.evasao; });
   
-  //extend frame height in `http://bl.ocks.org/`
-  d3.select(self.frameElement).style("height", "600px");  
-})();
-*/
+  // Mapeia os dados e obtém um vetor com o domínio de X (Anos)
+  // Domínio X = ["2009", "2010", "2011", "2012", "2013"]
+  var dominioAnos = dados.map(function(d) { return d.ano; }); 
+  
+  // Define as escalas do gráfico
+  var x = d3.scale.ordinal()
+    .domain(dominioAnos)
+    .rangeRoundBands([0, largura], .3);
+  
+  var y = d3.scale.linear()
+    .domain([0, valorMaximo])
+    .range([altura, 0]);
+  
+      
+  // Define os eixos do gráfico
+  var eixoX = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+    
+  var eixoY = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .ticks(5);
+    
+  
+  // Insere os eixos no gráfico
+  svg.append("g")
+      .attr("class", "x eixo")
+      .attr("transform", "translate(0," + altura + ")")
+      .call(eixoX); 
+  
+  svg.append("g")
+      .attr("class", "y eixo")
+      .call(eixoY); 
+    
+    
+  svg.selectAll("rect")
+    .attr("class", "barra")
+    .attr("x", function(d) { return x(d.ano); })
+    .attr("width", x.rangeBand())
+    .attr("y", function(d){ return y(d.evasao); })
+    .attr("height", function(d){return altura - y(d.evasao); })
+    .style("fill", "#00DEF2");
+
+}
+
+
+function Main(dados) {
+  var container = "#containerNorte";
+  CriaHeatMap(dados, container);
+  DesenhaHeatMap(container);
+}
